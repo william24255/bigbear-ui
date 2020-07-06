257c7a057e004175cd45f28eff85a7f4a4b3d4d8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-06 09:04:00 +0000

    Deploy yehuozhili/bigbear-ui to github.com/yehuozhili/bigbear-ui.git:gh-pages

---
+++ precache-manifest.e6aefc9bd49c69b1b77c96549cbf4869.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d7f3d65c76b5e46dccce4fc002461e",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main~24120820.e2b185ac763a63fdadb9.bundle.js"
+  },
+  {
+    "url": "runtime~main.e2b185ac763a63fdadb9.bundle.js"
+  },
+  {
+    "revision": "9758e86daed32ec364d9",
+    "url": "static/css/main~24120820.770c31fc.chunk.css"
+  },
+  {
+    "url": "vendors~main~0ad7406a.e2b185ac763a63fdadb9.bundle.js"
+  },
+  {
+    "url": "vendors~main~0ad7406a.e2b185ac763a63fdadb9.bundle.js.LICENSE.txt"
+  },
+  {
+    "url": "vendors~main~0decbd73.e2b185ac763a63fdadb9.bundle.js"
+  },
+  {
+    "url": "vendors~main~0decbd73.e2b185ac763a63fdadb9.bundle.js.LICENSE.txt"
+  },
+  {
+    "url": "vendors~main~0f485567.e2b185ac763a63fdadb9.bundle.js"
+  },
+  {
+    "url": "vendors~main~1f20a385.e2b185ac763a63fdadb9.bundle.js"
+  },
+  {
+    "url": "vendors~main~1f20a385.e2b185ac763a63fdadb9.bundle.js.LICENSE.txt"
+  },
+  {
+    "url": "vendors~main~2a42e354.e2b185ac763a63fdadb9.bundle.js"
+  },
+  {
+    "url": "vendors~main~678f84af.e2b185ac763a63fdadb9.bundle.js"
+  },
+  {
+    "url": "vendors~main~678f84af.e2b185ac763a63fdadb9.bundle.js.LICENSE.txt"
+  },
+  {
+    "url": "vendors~main~67b22fc5.e2b185ac763a63fdadb9.bundle.js"
+  },
+  {
+    "url": "vendors~main~95a414bc.e2b185ac763a63fdadb9.bundle.js"
+  },
+  {
+    "url": "vendors~main~b58f7129.e2b185ac763a63fdadb9.bundle.js"
+  },
+  {
+    "url": "vendors~main~b58f7129.e2b185ac763a63fdadb9.bundle.js.LICENSE.txt"
+  },
+  {
+    "url": "vendors~main~d939e436.e2b185ac763a63fdadb9.bundle.js"
+  },
+  {
+    "url": "vendors~main~d939e436.e2b185ac763a63fdadb9.bundle.js.LICENSE.txt"
+  },
+  {
+    "url": "vendors~main~ec8c427e.e2b185ac763a63fdadb9.bundle.js"
+  },
+  {
+    "url": "vendors~main~ec8c427e.e2b185ac763a63fdadb9.bundle.js.LICENSE.txt"
+  }
+]);
